45ff2157f3429707bd8fe76adaed96c7a22c126a
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2021-02-04 21:52:49 +0900

    main crawling추가

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,7 @@
+from blog.youtube_crawling import main_crawling
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'blog/home.html', {})
+    return main_crawling(request)
